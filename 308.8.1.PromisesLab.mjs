2e946535462db77802a7f62a7570b648fbb1562f
Jonathan Young <jy>
2024-10-07 16:23:57 -0700

    promise reject error handling

--- 308.8.1.PromisesLab.mjs
+++ 308.8.1.PromisesLab.mjs
@@ -1,8 +1,6 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./src/databases.mjs";
 
-
-
 async function getUserData(id) {
   let dbs = {
     db1: db1,
@@ -10,16 +8,16 @@ async function getUserData(id) {
     db3: db3,
   };
   try {
-  
-  const dbSelect = await central(id);
-  // const data = await dbs[dbSelect]
-  // const vaultInfo = await (vault(id))
-  //Promise all with destructuring
-  const [data, vaultInfo] = await Promise.all([dbs[dbSelect], vault(id)])
-  const dataObject = {id, ...data, ...vaultInfo}
-  console.log(dataObject)
-} catch (error) {
-  return Promise.reject(error)
-}
+    const dbSelect = await central(id);
+    if (!dbs[dbSelect]) {
+      return Promise.reject(Error(`${dbSelect} is down`));
+    }
+    //Promise all
+    const [data, vaultInfo] = await Promise.all([dbs[dbSelect](id), vault(id)]);
+    const dataObject = { id, ...data, ...vaultInfo };
+    console.log(dataObject);
+  } catch (error) {
+    return Promise.reject(error);
+  }
 }
 getUserData(4);
