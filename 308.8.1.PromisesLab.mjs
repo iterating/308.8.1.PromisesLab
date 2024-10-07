16a6882990c7f45209ac3b3da8a5c0c3940c8c28
Jonathan Young <jy>
2024-10-07 15:21:10 -0700

    pt 1

---
+++ 308.8.1.PromisesLab.mjs
@@ -0,0 +1,22 @@
+// Importing database functions. DO NOT MODIFY THIS LINE.
+import { central, db1, db2, db3, vault } from "./src/databases.mjs";
+
+
+
+async function getUserData(id) {
+  let dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3,
+  };
+  try {
+  
+  const dbSelect = await central(id);
+  const data = await dbs[dbSelect]
+  const vaultInfo = await (vault(id))c
+  console.log({id, ...data, vaultInfo})
+} catch (error) {
+  return Promise.reject(error)
+}
+}
+getUserData(1);
